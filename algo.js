3cc19da572682d1cda8cba2728f6c25b6445110c
neyneyalldayday <drfrankenstein123@gmail.com>
2022-04-21 18:32:51 -0500

    idk

---
+++ algo.js
@@ -0,0 +1,15 @@
+//write a function vowelCount that returns the number of vowels present in str
+
+
+
+function vowelCount(myStoopidString) {
+    let myStoopidString = 'yeahhhh bro im in the haooouuusse'
+    let stringArray =  myStoopidString.split("")
+    const newArray = []
+    for(i = 0; i < string.length; i++){
+        if(stringArray[i] === 'a' || stringArray[i] === 'e' || stringArray[i] === 'i' || stringArray[i] === 'o' || stringArray[i] === 'u'){
+            newArray.push(stringArray[i])
+        }
+        console.log(newArray[i])
+    
+}
