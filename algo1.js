84c0081c096dc945695c6d8ea33c365c7af7d6de
neyneyalldayday <drfrankenstein123@gmail.com>
2022-04-26 19:01:33 -0500

    wooosh

---
+++ algo1.js
@@ -0,0 +1,15 @@
+// Write a function linearSearch() that 
+// - accepts an array of numbers, and a target number
+// - returns the index of the target number if it is present in the array
+// - otherwise, returns -1
+
+// Example:
+// linearSearch([82, 22, -4, 21, 1, 8], 8)
+// returns: 5 (position of number 8 in the array)
+
+arr = [23, 34,5,6,7,12,56]
+function linearSearch(arr) {
+ for (let i = 0; i < arr[i].length; i++) {
+     
+ }
+}
